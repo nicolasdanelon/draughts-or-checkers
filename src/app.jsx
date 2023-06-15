3bd78fad2bc161578c54df87d413bbc3e9399e69
Nicolas Danelon <nicolasdanelon@gmail.com>
2023-06-15 13:55:17 -0300

    🚀 First commit react version

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'preact/hooks'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="wrapper-board">
+      </div>
+    </>
+  )
+}
