48450b6a1d91b015005837f57476b981da18b479
Nicolas Danelon <nicolasdanelon@gmail.com>
2023-06-15 14:48:46 -0300

    ✨ Adding game board & square components

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'preact/hooks'
+import useCheckerStore from './store'
+
+const Square = ({ color }) => {
+  return <div class={`square ${color}-square`}>{"\u00A0"}</div>
+}
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const { gameBoard, checkCell } = useCheckerStore();
 
   return (
-    <>
-      <div class="wrapper-board">
-      </div>
-    </>
+    <div class="wrapper-board">
+      {gameBoard.map((row, i) => {
+        return row.map((col, j) => {
+          const color = (i + j) % 2 === 0 ? 'black' : 'white';
+
+          return (
+            <Square color={color} />
+          );
+        })
+      })}
+    </div >
   )
 }
