ce71833ffb2b3cf639a1c18d0cbc382321619fc7
Nicolas Danelon <nicolasdanelon@gmail.com>
2023-06-15 21:50:54 -0300

    💄 Better styles for checker

--- src/components/checker.jsx
+++ src/components/checker.jsx
@@ -4,9 +4,15 @@ const Checker = ({ color }) => (
       w-8 h-8 rounded-full
       relative top-1/2 left-1/2
       -translate-x-2/4 -translate-y-2/4
+      border-2 border-solid border-gray-600
     "
     style={{ backgroundColor: color }}
-  />
+  >
+    <div class="
+      w-4 h-4 rounded-full m-1 -rotate-45
+      border-2 border-solid border-gray-400 border-x-0 border-b-0
+    " />
+  </div>
 )
 
 export default Checker;
